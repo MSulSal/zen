f5ad40338c60489c3fd8fb06ea75aa19ed214e85
MSulSal <msulsaleem@gmail.com>
2025-04-24 14:43:21 -0500

    randomized colors

---
+++ script.js
@@ -0,0 +1,41 @@
+const wrapper = document.getElementById("wrapper");
+const shapes = document.getElementsByClassName("shape");
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const uniqueRand = (min, max, prev) => {
+  let next = prev;
+
+  while (prev === next) next = rand(min, max);
+
+  return next;
+};
+
+const combinations = [
+  { configuration: 1, roundness: 1 },
+  { configuration: 1, roundness: 2 },
+  { configuration: 1, roundness: 4 },
+  { configuration: 2, roundness: 2 },
+  { configuration: 2, roundness: 3 },
+  { configuration: 3, roundness: 3 },
+];
+
+let prev = 0;
+
+setInterval(() => {
+  const index = uniqueRand(0, combinations.length - 1, prev),
+    combination = combinations[index];
+
+  wrapper.dataset.configuration = combination.configuration;
+  wrapper.dataset.roundness = combination.roundness;
+
+  prev = index;
+
+  for (let shape of shapes) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const a = Math.random().toFixed(2);
+    shape.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+  }
+}, 2000);
